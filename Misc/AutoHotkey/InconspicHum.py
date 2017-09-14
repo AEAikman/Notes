9c1bef263a8a772cee89f6a56593da7dd4647b0d
AEAikman <aaikman@halon.com>
2017-09-14 10:22:43 -0700

    General Commit

--- Misc/AutoHotkey/InconspicHum.py
+++ Misc/AutoHotkey/InconspicHum.py
@@ -1,6 +1,6 @@
 
 
-import praw
+import praw, random
 
 bot = praw.Reddit(user_agent='InconspicuousHuman',
                   client_id='h14_mF9hf1LIfQ',
@@ -12,15 +12,20 @@ bot = praw.Reddit(user_agent='InconspicuousHuman',
 
 StrToFind = "every account on reddit is a bot except you."
 
-message = "and also me..."
+PossibleMessages = ["and also me...", "and inconspicuous humans...", "Well at least I'm not a bot...", "Every bot on reddit is you.", "Every account on reddit is a bot, including you.", "I forgot I was supposed to say."]
 
 subreddit = bot.subreddit('awesomebots')
 
 comments = subreddit.stream.comments()
 
+AlreadyRepliedComments = []
+
 for comment in comments:
     text = comment.body # Fetch body
     author = comment.author # Fetch author
-    if StrToFind in text.lower():
-        # Generate a message
-        comment.reply(message) # Send message
+    if comment not in AlreadyRepliedComments:
+	    if StrToFind in text.lower():
+	        # Generate a message
+	        message = random.choice(PossibleMessages)
+	        comment.reply(message) # Send message
+	        AlreadyRepliedComments.append(comment)
